945ffddc88f44893560c68fd048e879fd074b5d3
Fernanda Montalvan <125288587+Ferzk98@users.noreply.github.com>
2023-11-01 11:02:20 +0100

    Updating code

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-// Returns a random DNA base
+// Helper function to generate a random base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
+  const bases = ['A', 'T', 'C', 'G'];
+  const randomIndex = Math.floor(Math.random() * bases.length);
+  return bases[randomIndex];
 };
 
-// Returns a random single stand of DNA containing 15 bases
+// Helper function to create a mock DNA strand with 15 bases
 const mockUpStrand = () => {
   const newStrand = [];
   for (let i = 0; i < 15; i++) {
@@ -14,9 +15,48 @@ const mockUpStrand = () => {
 };
 
 
+const pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum: specimenNum,
+    dna: dna,
+    // Method to simulate a mutation
+    mutate() {
+      const randomIndex = Math.floor(Math.random() * this.dna.length);
+      let newBase = returnRandBase();
+      while (newBase === this.dna[randomIndex]) {
+        newBase = returnRandBase();
+      }
+      this.dna[randomIndex] = newBase;
+      return this.dna;
+    },
+
+    compareDNA(pAequor) {
+      let identicalBases = 0;
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === pAequor.dna[i]) {
+          identicalBases++;
+        }
+      }
+      const percentage = (identicalBases / this.dna.length) * 100;
+      console.log(`Specimen #${this.specimenNum} and Specimen #${pAequor.specimenNum} have ${percentage.toFixed(2)}% DNA in common.`);
+    },
+
+    willLikelySurvive() {
+      const cgCount = this.dna.filter(base => base === 'C' || base === 'G').length;
+      const percentage = (cgCount / this.dna.length) * 100;
+      return percentage >= 60;
+    }
+  };
+};
 
 
-
-
-
-
+const pAequorArray = [];
+let specimenNum = 1;
+while (pAequorArray.length < 30) {
+  const newDna = mockUpStrand();
+  const newOrganism = pAequorFactory(specimenNum, newDna);
+  if (newOrganism.willLikelySurvive()) {
+    pAequorArray.push(newOrganism);
+    specimenNum++;
+  }
+}
